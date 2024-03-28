27fcf9234bd6edb25186434dadcaf0bc2745f31b
Pedro Santos <pedrosantos.tds2.senai@gmail.com>
2024-03-28 08:38:47 -0300

    nsnfdnvn

--- b.js
+++ b.js
@@ -1,5 +1,6 @@
 const animals = ['ant', 'bison', 'camel', 'duck', 'elephant', 
-'turtle', 'tiger', 'lion']
-console.log(animals.slice(2))
-console.log(animals.slice(2, -1))
-console.log(animals.slice())
+'turtle', 'tiger', 'lion'];
+console.log(animals.slice(2));
+console.log(animals.slice(2, -1));
+console.log(animals.slice());
+//dadsdasdadasad
